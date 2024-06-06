77366c2bb8687550052d3c3613ba3ece5e12809d
MAMCB <96302464+MAMCB@users.noreply.github.com>
2024-06-06 15:57:37 +0200

    created new routes, new views and setup tailwind

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    return view('welcome');
+   return view('welcome');
+   
+});
+
+Route::get('/main', function () {
+    return view('main');
+});
+
+Route::get("/hello", function(){
+    return redirect("/greet/World");
+})->name("hello");
+
+Route::get("hallo", function(){
+    return redirect()->route("hello");
+});
+
+Route::get("/greet/{name}", function($name){
+    return "Hello, $name";
+});
+
+Route::fallback(function(){
+    return view("fallback");
 });
