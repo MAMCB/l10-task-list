c2ad1f6c591fa43626aedf6f96774edbd41f1568
MAMCB <96302464+MAMCB@users.noreply.github.com>
2024-06-07 13:00:17 +0200

    created tasks index route and tasks show route, defined blade templates for each route

--- resources/views/task.blade.php
+++ resources/views/task.blade.php
@@ -15,7 +15,7 @@
     </head>
     <body>
         <section class="bg-gray-400 p-9 rounded-lg">
-            <h1 class="text-6xl p-6">List of tasks</h1>
+            <h1 class="text-6xl p-6">{{$task->title}}</h1>
           
 
             
@@ -24,17 +24,12 @@
         <section>
             
              
-                    @forelse($tasks as $task)
+                    
                         <div>
-                            <a href="{{route("tasks.show",["id"=>$task->id])}}">
-                                <h1 class="text-4xl">{{$task->title}}</h1>
-                            </a>
-                            
-                            <p>{{$task->description}}</p>
+                           
+                            <p>{{$task->long_description?($task->long_description):($task->description)}}</p>
                         </div>
-                        @empty
-                        <p>No tasks available</p>
-                    @endforelse
+                       
                 
             
            
--- routes/web.php
+++ routes/web.php
@@ -2,27 +2,77 @@
 
 use Illuminate\Support\Facades\Route;
 
+class Task
+{
+  public function __construct(
+    public int $id,
+    public string $title,
+    public string $description,
+    public ?string $long_description,
+    public bool $completed,
+    public string $created_at,
+    public string $updated_at
+  ) {
+  }
+}
 
-Route::get('/', function () {
-   return view('index',['name' => 'Miguel']);
+$tasks = [
+  new Task(
+    1,
+    'Buy groceries',
+    'Task 1 description',
+    'Task 1 long description',
+    false,
+    '2023-03-01 12:00:00',
+    '2023-03-01 12:00:00'
+  ),
+  new Task(
+    2,
+    'Sell old stuff',
+    'Task 2 description',
+    null,
+    false,
+    '2023-03-02 12:00:00',
+    '2023-03-02 12:00:00'
+  ),
+  new Task(
+    3,
+    'Learn programming',
+    'Task 3 description',
+    'Task 3 long description',
+    true,
+    '2023-03-03 12:00:00',
+    '2023-03-03 12:00:00'
+  ),
+  new Task(
+    4,
+    'Take dogs for a walk',
+    'Task 4 description',
+    null,
+    false,
+    '2023-03-04 12:00:00',
+    '2023-03-04 12:00:00'
+  ),
+];
+
+$tasks2 = [];
+
+
+Route::get('/', function () use($tasks) {
+   return view('index',['tasks' => $tasks]);
    
-});
+})->name("tasks.index");
 
-Route::get('/main', function () {
-    return view('main');
-});
-
-Route::get("/hello", function(){
-    return redirect("/greet/World");
-})->name("hello");
-
-Route::get("hallo", function(){
-    return redirect()->route("hello");
-});
-
-Route::get("/greet/{name}", function($name){
-    return "Hello, $name";
-});
+Route::get('/tasks/{id}', function ($id) use($tasks) {
+    $task = null;
+    foreach($tasks as $t){
+        if($t->id == (int)$id){
+            $task = $t;
+            break;
+        }
+    }
+    return view('task',['task' => $task]);
+})->name('tasks.show');
 
 Route::fallback(function(){
     return view("fallback");
