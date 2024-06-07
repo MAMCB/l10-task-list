ed7f65a05b21ed3662d7bdf747ada2ea16c49d24
MAMCB <96302464+MAMCB@users.noreply.github.com>
2024-06-07 13:27:30 +0200

    refactored fallback template to extend from layout

--- resources/views/fallback.blade.php
+++ resources/views/fallback.blade.php
@@ -1,21 +1,6 @@
-<html>
-    
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Main</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-        <!-- Styles -->
-       @vite('resources/css/app.css')
-    </head>
-    <body>
+@extends('layouts.app')
+@section('content')
         <div class="bg-red-500 rounded-lg p-6" >
             <h1 class="text-6xl p-6">Oops, it seems this page doesn't exist</h1>
         </div>
-    </body>
-</html>
+@endsection
